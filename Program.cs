d765bf107439840b4ed5da147e1f4c3298d8c5fe
Farzaneh1359 <66690179+Farzaneh1359@users.noreply.github.com>
2020-06-16 13:27:38 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyText="c# Exercise";
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("programming in C#");
+            Console.WriteLine(MyText);
+            MyText = "C# Exercise2";
+            Console.WriteLine(MyText);
+            Console.ReadLine();
+
+        }
+    }
+}
